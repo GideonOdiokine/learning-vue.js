8da70412d8fbf9b4916fb9d6c1a15b23df63eabf
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-25 23:42:38 +0100

    learning methods in vue,js

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
-  <h2 v-for="(name, index) in names" :key="name">{{ index + 1 }} {{ name }}</h2>
-  <h2 v-for="name in fullnames" key="name.first">
-    {{ name.first }} {{ name.last }}
-  </h2>
 
-  <div v-for="actor in actors" >
-    {{actor.name}}
-    <h6 v-for="movie in actor.movies" >{{movie}}</h6>
-  </div>
- <h4 v-for="(value, key,index) in myInfo">
-  {{index + 1}} {{key}}  {{value}}
- </h4>
 </template>
 
 <script>
@@ -18,21 +7,7 @@ export default {
   name: "App",
   data() {
     return {
-      names: ["Gideon", "Odiokine", "Nancy"],
-      fullnames: [
-        { first: "Gideon", last: "Odiokine" },
-        { first: "Nancy", last: "Asiuloka" },
-        { first: "Favour", last: "Odiokine" },
-      ],
-      actors: [
-        { name: "Gideon Odiokine", movies: ["Princess", "Diana"] },
-        { name: "John Odiokine", movies: ["Lovery", "Army of Death"] },
-      ],
-      myInfo:{
-        name:'Gideon',
-        channel:"Gidicode",
-        course:"Vue.js 3"
-      }
+     
     };
   },
 };
